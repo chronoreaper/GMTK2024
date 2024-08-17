e2d855b28fd9c2b11ee16151bf4d2d503e85cc61
chronoreaper <chris.dd.29@gmail.com>
2024-08-17 13:15:42 -0400

    Created Base Object

--- Assets/Scripts/UI/SpawnFromButton.cs
+++ Assets/Scripts/UI/SpawnFromButton.cs
@@ -2,5 +2,19 @@ using UnityEngine;
 
 public class SpawnFromButton : MonoBehaviour
 {
-    public void Spawn(GameObject prefab) => Instantiate(prefab, Vector3.zero, Quaternion.identity);
+    private GameObject _spawnedObject = null;
+    public void Spawn(GameObject prefab)
+    {
+        if (_spawnedObject != null)
+            return;
+        _spawnedObject = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+    }
+
+    public void DisableUI(bool disable)
+    {
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(!disable);
+        }
+    }
 }
